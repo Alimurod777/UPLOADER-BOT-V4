8f6cfeaffb1e0c65fa468baf1c1e502d2dd61305
Alimurod777 <aliturdaliyev777@gmail.com>
2024-12-13 23:53:44 +0500

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -13,7 +13,7 @@ class Config(object):
     
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     
-    API_ID = int(os.environ.get("API_ID", ""))
+    API_ID = int(os.environ.get("API_ID", "11411895"))
     
     API_HASH = os.environ.get("API_HASH", "")
     
